fec543fedfe1124d641502b55fa3d95d2826cafe
Niko Giraud <niamke.giraud@gmail.com>
2018-03-29 08:50:53 -0400

    Revised models based on Alan's new updated diagram
    Created a new branch since this may cause conflicts with existing templates and urls. Should be easy to merge back with original models branch. Don't stay in this branch for long after configuration.

--- dndev/dndev/press_f_to_pay_respects/dndev/admin.py
+++ dndev/dndev/press_f_to_pay_respects/dndev/admin.py
@@ -19,11 +19,10 @@ admin.site.register(CharacterSubclassSpellList)
 admin.site.register(CharacterClassStartingEquipment)
 admin.site.register(Spells)
 
-from .models import FeatRanking, Feat, Weapon, Armor, Properties, Equipment, Money
+from .models import FeatRanking, Feat, Weapon, Armor, Properties, Equipment
 admin.site.register(FeatRanking)
 admin.site.register(Feat)
 admin.site.register(Properties)
-admin.site.register(Money)
 admin.site.register(Armor)
 admin.site.register(Equipment)
 admin.site.register(Weapon)
