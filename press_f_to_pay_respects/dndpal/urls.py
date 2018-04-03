a1aa4f82dde8aff0573e1b3e3f71042b01b01aba
Manny <manuel.moquete.1994@gmail.com>
2018-04-03 17:04:54 -0400

    Organizing

---
+++ press_f_to_pay_respects/dndpal/urls.py
@@ -0,0 +1,24 @@
+
+from django.urls import path, include, re_path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('simple/', views.simple, name='simple'),
+    path('armors/', views.ArmorListView.as_view(), name='armors'),
+    path('armor/<str:pk>', views.ArmorDetailView.as_view(), name='armor-detail'),
+    path('classes/', views.ClassListView.as_view(), name='classes'),
+    path('class/<str:pk>', views.ClassDetailView.as_view(), name='class-detail'),
+    path('equipment/', views.EquipmentListView.as_view(), name='equipment'),
+    path('equipment/<str:pk>', views.EquipmentDetailView.as_view(), name='equipment-detail'),
+    path('races/', views.RaceListView.as_view(), name='races'),
+    path('race/<str:pk>', views.RaceDetailView.as_view(), name='race-detail'),
+    path('weapons/', views.WeaponListView.as_view(), name='weapons'),
+    path('weapon/<str:pk>', views.WeaponDetailView.as_view(), name='weapon-detail'),
+    path('spells/', views.SpellListView.as_view(), name='spells'),
+    path('spell/<str:pk>', views.SpellDetailView.as_view(), name='spell-detail'),
+    path('classes/', views.ClassListView.as_view(), name='classes'),
+    path('class/<str:pk>', views.ClassDetailView.as_view(), name='class-detail'),
+
+]
---
+++ press_f_to_pay_respects/dndpal/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from .models import *
+
+def home(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_armors=Armor.objects.all().count()
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'home.html',
+        context={'num_armors':num_armors},
+    )
+
+
+def simple(request):
+    """
+    View function for simple page of site.
+    """
+    # Generate data needed for page
+
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'simple.html',
+    )
+
+from django.views import generic
+from django.db.models import F
+
+
+class ArmorListView(generic.ListView):
+    model = Armor
+
+class ArmorDetailView(generic.DetailView):
+    model = Armor
+
+class ClassListView(generic.ListView):
+    model = CharacterClass
+
+class ClassDetailView(generic.DetailView):
+    model = CharacterClass
+
+class EquipmentListView(generic.ListView):
+    model = Equipment
+
+class EquipmentDetailView(generic.DetailView):
+    model = Equipment
+
+class RaceListView(generic.ListView):
+    model = Race
+
+class RaceDetailView(generic.DetailView):
+    model = Race
+
+    def get_context_data(self, **kwargs):
+        context = super(RaceDetailView, self).get_context_data(**kwargs)
+        context['raceFeatures'] = RaceFeatures.objects.all()
+        return context
+
+class WeaponListView(generic.ListView):
+    model = Weapon
+
+class WeaponDetailView(generic.DetailView):
+    model = Weapon
+
+class SpellListView(generic.ListView):
+    model = Spells
+
+class SpellDetailView(generic.DetailView):
+    model = Spells
+
+class ClassListView(generic.ListView):
+    model = CharacterClass
+
+class ClassDetailView(generic.DetailView):
+    model = CharacterClass
