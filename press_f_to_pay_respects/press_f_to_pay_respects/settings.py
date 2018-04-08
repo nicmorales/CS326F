c12edcf30d16a484e1b41b65e16efc76541502c8
Manny <manuel.moquete.1994@gmail.com>
2018-04-07 23:56:10 -0400

    Authenticaion
    Added authentication, some styling to the pages to come later this week.

--- press_f_to_pay_respects/press_f_to_pay_respects/settings.py
+++ press_f_to_pay_respects/press_f_to_pay_respects/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'press_f_to_pay_respects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
