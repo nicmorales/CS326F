0dc2f27b92bccb242439caeee523ef399907ab31
Niko Giraud <niamkegiraud@gmail.com>
2018-04-27 03:50:04 -0400

    Removed venv

--- venv/Lib/site-packages/material_widgets/static/material_widgets/js/material_radio.js
+++
@@ -1,4 +0,0 @@
-let radios = document.querySelectorAll('.mdc-radio');
-for (let i = 0, radio; radio = radios[i]; i++) {
-  mdc.radio.MDCRadio.attachTo(radio);
-}
--- venv/Lib/site-packages/material_widgets/static/material_widgets/js/material_slider.js
+++
@@ -1,5 +0,0 @@
-let sliders = document.querySelectorAll('.mdc-slider');
-for (let i = 0, slider; slider = sliders[i]; i++) {
-  let material_slider = new mdc.slider.MDCSlider(slider);
-  material_slider.listen('MDCSlider:change', () => document.getElementById(slider.dataset.id).value = material_slider.value);
-}
