8deb4413ac690eafa23cdb765205c353d598521e
Niko Giraud <niamkegiraud@gmail.com>
2018-04-28 05:36:50 -0400

    Deleted venv, added ui base images

--- venv/Lib/site-packages/material_widgets/static/material_widgets/js/material_button.js
+++
@@ -1,4 +0,0 @@
-let buttons = document.querySelectorAll('.mdc-button');
-for (let i = 0, button; button= buttons[i]; i++) {
-  mdc.ripple.MDCRipple.attachTo(button);
-}
--- venv/Lib/site-packages/material_widgets/static/material_widgets/js/material_checkbox.js
+++
@@ -1,4 +0,0 @@
-let checkboxes = document.querySelectorAll('.mdc-checkbox');
-for (let i = 0, checkbox; checkbox = checkboxes[i]; i++) {
-  mdc.checkbox.MDCCheckbox.attachTo(checkbox);
-}
